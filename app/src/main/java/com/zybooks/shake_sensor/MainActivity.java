ecc44a5ac2c575dc62ab61ce13f3d906b0177181
Waterfighter1 <chloe.gertner.cg@gmail.com>
2023-04-17 08:20:58 -0500

    Color Chance

--- app/src/main/java/com/zybooks/shake_sensor/MainActivity.java
+++ app/src/main/java/com/zybooks/shake_sensor/MainActivity.java
@@ -22,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // Main Text
         TextView mainTxt = findViewById(R.id.txt_main);
         Button btn = findViewById(R.id.button);
